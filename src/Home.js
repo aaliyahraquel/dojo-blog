44bce9a3b3184c05972b25be5fccab1380886061
Aaliyah.johnson <aaliyah.johnson@comparethemarket.com>
2024-03-24 00:21:08 +0000

    add anon function and event argument

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello')
+  }
+
+  const handleClickAgain = (name,e) => {
+    console.log('hello' + name, e.target)
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me!</button>
+      <button onClick={(e) => {
+        handleClickAgain('mario', e) // creates an anonymous function which only gets invoked onClick
+      }}>Click me again</button>
     </div>
    );
 }
