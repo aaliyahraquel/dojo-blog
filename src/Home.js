dd65121227cb0881575343efbc67a701c2e4ae81
Aaliyah.johnson <aaliyah.johnson@comparethemarket.com>
2024-04-22 23:29:44 +0100

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,26 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlog] = useState(null); // set to null because data is now returned from json server
+  const [isLoading, setIsLoading] = useState(true); //to let user know if the fetch is taking a while
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs') // promise
-    .then(res => {
-      return res.json(); // async promise
-    })
-    .then(data => { // promise
-      console.log(data);
-      setBlog(data);
-    })
+    setTimeout(() => { // simulate a real fetch request - conditional loading message
+      fetch('http://localhost:8000/blogs') // promise
+      .then(res => {
+        return res.json(); // async promise
+      })
+      .then(data => { // promise
+        console.log(data);
+        setBlog(data);
+        setIsLoading(false) //once the data has loaded, it is no longer loading
+      })
+    }, 1000)
   }, []); // an empty dependency array only allows the useEffect hook to render on the first initial render, ONLY
   // when there's a value in the array, the page renders upon the cnage of state of that value
 
   return ( 
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && < BlogList blogs={ blogs }  title= 'All Blogs'/>} 
     </div>
    )
