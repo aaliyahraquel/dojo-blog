5be0576ec5dfe71bdaee1f1fd46f798711c11ed5
Aaliyah.johnson <aaliyah.johnson@comparethemarket.com>
2024-04-23 21:51:19 +0100

    added error handling

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlog] = useState(null); // set to null because data is now returned from json server
   const [isLoading, setIsLoading] = useState(true); //to let user know if the fetch is taking a while
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     setTimeout(() => { // simulate a real fetch request - conditional loading message
       fetch('http://localhost:8000/blogs') // promise
-      .then(res => {
-        return res.json(); // async promise
-      })
-      .then(data => { // promise
-        console.log(data);
-        setBlog(data);
-        setIsLoading(false) //once the data has loaded, it is no longer loading
-      })
+        .then(res => {
+          if(!res.ok) { //check response object
+            throw Error('could not fetch data for that resorce'); //throw error for any other type of error except 5xx
+          }
+          return res.json(); // async promise
+        })
+        .then(data => { // promise
+          setBlog(data);
+          setIsLoading(false) //once the data has loaded, it is no longer loading
+          setError(null);
+        })
+        .catch(err => {
+          setIsLoading(false)
+          setError(err.message) //catch any network error
+        })
     }, 1000)
   }, []); // an empty dependency array only allows the useEffect hook to render on the first initial render, ONLY
   // when there's a value in the array, the page renders upon the cnage of state of that value
 
   return ( 
     <div className="home">
+      {error && <div> { error } </div>}
       {isLoading && <div>Loading...</div>}
       {blogs && < BlogList blogs={ blogs }  title= 'All Blogs'/>} 
     </div>
