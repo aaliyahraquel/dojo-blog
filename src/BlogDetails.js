8e47cdf8d516f618561dbfb7acbf16e8eef35986
Aaliyah.johnson <aaliyah.johnson@comparethemarket.com>
2024-04-25 23:29:29 +0100

    deleting a blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from 'react-router-dom';
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
 
 
 const BlogDetails = () => {
   const { id } = useParams(); // allows us to grab route params from the url
   const { data: blog, isLoading, error } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
   
+  const handleClick = (e) => {
+    fetch('http://localhost:8000/blogs/' + id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/');
+    })
+  }
+
   return ( 
     <div className="blog-details">
       {isLoading && <div>Loading...</div>}
@@ -15,6 +25,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
 
